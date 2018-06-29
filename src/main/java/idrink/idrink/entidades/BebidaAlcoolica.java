9ff312068c4c7f8974d5b4ac0a446771e0227515
bianca <bianca@bianca>
2018-06-28 23:08:02 -0300

    NamedQueries implementadas

--- src/main/java/idrink/idrink/entidades/BebidaAlcoolica.java
+++ src/main/java/idrink/idrink/entidades/BebidaAlcoolica.java
@@ -9,6 +9,8 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 /**
  *
@@ -16,8 +18,22 @@ import javax.persistence.Entity;
  */
 @Entity
 @DiscriminatorValue(value = "SIM")
+@NamedQueries(
+        {
+            @NamedQuery(
+                    name = BebidaAlcoolica.BEBIDAS_POR_TEOR,
+                    query = "SELECT ba.nome, ba.teor, ba.preco FROM BebidaAlcoolica ba ORDER BY ba.teor"
+            ),
+            @NamedQuery(
+                    name = BebidaAlcoolica.BEBIDAS_ALCOOLICAS,
+                    query = "SELECT ba FROM BebidaAlcoolica ba ORDER BY ba.id"
+            )
+        }
+)
 public class BebidaAlcoolica extends Bebida implements Serializable {
     
+    public static final String BEBIDAS_POR_TEOR = "selecionarPorTeor";
+    public static final String BEBIDAS_ALCOOLICAS = "bebidasAlcoolicas";
     @Column(name = "PERCENT_TEOR_ALCOOL", nullable = true)
     private Float teor;
 
