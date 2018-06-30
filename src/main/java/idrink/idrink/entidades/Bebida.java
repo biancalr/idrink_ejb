cbe510c75317d06090085c378307066d97341837
bianca <bianca@bianca>
2018-06-29 23:03:43 -0300

    SetId adicionado

--- src/main/java/idrink/idrink/entidades/Bebida.java
+++ src/main/java/idrink/idrink/entidades/Bebida.java
@@ -78,6 +78,10 @@ public class Bebida implements Serializable {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getNome() {
         return nome;
     }
